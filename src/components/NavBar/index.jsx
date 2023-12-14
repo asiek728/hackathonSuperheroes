49199e5d26cdac29ef3804265d1699677cfb14a0
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-12-14 15:42:05 +0000

    css fix

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+import './index.css'
 
 const NavBar = () => {
   return (
     <main>
     <header>
         <nav>
+          <div>
+            <NavLink to="/"><h1 style={{color: "gold"}}>Assemble</h1></NavLink>
+          </div>
+          <div className="nav-links">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/heroes">Heroes</NavLink>
+          </div>
         </nav>
     </header>
     <Outlet />
